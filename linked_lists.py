40b3df5a5d0e5e616bf50958b26f8035b32357a2
Mark Jumba <jumbamark@yahoo.com>
2022-05-05 13:54:09 +0300

    creating a linked list with four nodes

--- linked_lists.py
+++ linked_lists.py
@@ -1,13 +1,37 @@
-# Implementing your own linked list
+# creating a linked list with four nodes
 
-
-# creating a class to represent the linked list (only store where the list starts, the head of the list)
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-# class to represent each node of the linked list
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+    def __repr__(self):
+        return self.data
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+l_list = LinkedList()
+print(l_list)
+
+first_node = Node("200")
+l_list.head = first_node
+
+second_node = Node("Lynne")
+third_node = Node("611")
+fourth_node = Node("Mercy")
+first_node.next = second_node
+second_node.next = third_node
+third_node.next = fourth_node
+print(l_list)
